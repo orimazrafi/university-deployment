261c94a404073023380622185c0a06404b02d553
ori mazrafi <orimazrafi@gmail.com>
2020-02-24 19:20:24 +0200

    s

---
+++ public/build/precache-manifest.5948b0305c4d433a927ec29da5cd2054.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698e9cc65307c654661a52986067897f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c40f5866a3dc3386c93",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "9389260d5a53f92fd805",
+    "url": "/static/css/main.1a4fec87.chunk.css"
+  },
+  {
+    "revision": "2c40f5866a3dc3386c93",
+    "url": "/static/js/2.1d070058.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/static/js/2.1d070058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9389260d5a53f92fd805",
+    "url": "/static/js/main.0a725e31.chunk.js"
+  },
+  {
+    "revision": "6cba60af8b28124fc195",
+    "url": "/static/js/runtime-main.4278802a.js"
+  }
+]);
