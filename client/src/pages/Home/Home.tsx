248dfbc85d519f022d066293359fec043e768612
orimazrafi <orimazrafi@gmail.com>
2020-03-18 18:52:17 +0200

    Handle home image.

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,11 +1,4 @@
 import React from "react";
 import "./Home.css";
-import ReactPlayer from "react-player";
 
-export const Home = () => (
-  <ReactPlayer
-    url={process.env.REACT_APP_CLOUDINARY_MP4_VIDEO}
-    style={{ height: "100px", width: "200px" }}
-    loop
-  />
-);
+export const Home = () => <div>Home</div>;
