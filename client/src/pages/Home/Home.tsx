1883226bc0b0c99e199b8ce772f4e92794458820
orimazrafi <orimazrafi@gmail.com>
2020-03-18 18:02:25 +0200

    handle background image.

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
 export const Home = () => (
-    <div >
-        Home
-    </div>
-)
+  <video className="home__full_screen_video" poster="media/foo.jpg" loop>
+    <source src={process.env.REACT_APP_CLOUDINARY_MP4_VIDEO} type="video/mp4" />
+  </video>
+);
--- client/src/pages/Logout/Logout.tsx
+++ client/src/pages/Logout/Logout.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 export const Logout = () => {
-    React.useEffect(() => {
-        localStorage.removeItem('credentials');
-        return window.location.replace("/auth")
-    }
-        , []);
-    return null
-}
+  React.useEffect(() => {
+    localStorage.removeItem("credentials");
+    return window.location.replace("/");
+  }, []);
+  return null;
+};
