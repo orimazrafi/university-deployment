232a5a34aa93a886034499054fc5fda529442214
orimazrafi <orimazrafi@gmail.com>
2020-03-19 08:47:55 +0200

    Handle background video.

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,8 +3,18 @@ import "./Home.css";
 import ReactPlayer from "react-player";
 
 export const Home = () => (
-  <div>
+  <div
+    style={{
+      position: "absolute",
+      top: "60",
+      left: "0",
+      right: "0",
+      bottom: "100px"
+    }}
+  >
     <ReactPlayer
+      height="100%"
+      width="100%"
       url={process.env.REACT_APP_CLOUDINARY_MP4_VIDEO}
       playing
       loop
